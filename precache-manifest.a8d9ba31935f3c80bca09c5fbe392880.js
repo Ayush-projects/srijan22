27dbce17f445aafdf4eeb0d5f540705a5e7f32ff
Ayush <kayush479@gmail.com>
2022-03-07 00:27:46 +0530

    Updates

---
+++ precache-manifest.a8d9ba31935f3c80bca09c5fbe392880.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f264a92abc93f88376b8e993d297696b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34d5cde29aa251ea0471",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "597cf92d7956edd73fd2",
+    "url": "/static/css/main.2aa47c52.chunk.css"
+  },
+  {
+    "revision": "34d5cde29aa251ea0471",
+    "url": "/static/js/2.6f62d62f.chunk.js"
+  },
+  {
+    "revision": "ce82387e16470a2151bb32ebda9e3277",
+    "url": "/static/js/2.6f62d62f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "597cf92d7956edd73fd2",
+    "url": "/static/js/main.580036d9.chunk.js"
+  },
+  {
+    "revision": "a92017c60e7effd99e19",
+    "url": "/static/js/runtime-main.759aadff.js"
+  },
+  {
+    "revision": "b4f5655745d1cf7e93605d688629cdee",
+    "url": "/static/media/1200px-GAIL_Logo.svg.b4f56557.png"
+  },
+  {
+    "revision": "b4310cff60b2768e87b5d8a6ac232d7e",
+    "url": "/static/media/1280px-Hyundai_Motor_Company_logo.svg.b4310cff.png"
+  },
+  {
+    "revision": "ce2963f802791ca5f5fec2430a195280",
+    "url": "/static/media/ATKT Logo 2.ce2963f8.png"
+  },
+  {
+    "revision": "3932bb48ab4ac1c5be8c01628e10d768",
+    "url": "/static/media/Erudite Logo.3932bb48.jpg"
+  },
+  {
+    "revision": "501fd3be289e9f95e6109dfb7d2646d9",
+    "url": "/static/media/F5_poster_1.501fd3be.jpeg"
+  },
+  {
+    "revision": "50d56d3419e8b5c98566f5bb59a4eb26",
+    "url": "/static/media/F5_poster_3.50d56d34.jpeg"
+  },
+  {
+    "revision": "c2770788422423e8ff3a671b7446a989",
+    "url": "/static/media/Jadavpur_University_Logo.c2770788.svg"
+  },
+  {
+    "revision": "b1a26c23c895c119de2edf53f95a91fb",
+    "url": "/static/media/MY PRINT STORY LOGO-min.b1a26c23.jpg"
+  },
+  {
+    "revision": "b901a55a48b84ccf2e60191cf8744d98",
+    "url": "/static/media/SCH1-min.b901a55a.jpg"
+  },
+  {
+    "revision": "ef10df658116446d1a6b1ac6a4fef4b7",
+    "url": "/static/media/Webel Fuji Logo.ef10df65.png"
+  },
+  {
+    "revision": "cda0e4c7fb5ea4b9ff46cde658913f69",
+    "url": "/static/media/ZDS Icon.cda0e4c7.jpeg"
+  },
+  {
+    "revision": "c4284d3f4ea54501ed30ceeccbd5664d",
+    "url": "/static/media/app-min.c4284d3f.png"
+  },
+  {
+    "revision": "48e368f0ed2ee3fb90aa2d0560282f98",
+    "url": "/static/media/bg-landing.48e368f0.jpg"
+  },
+  {
+    "revision": "cd004a777c6f584dc7e40e8fff34f04b",
+    "url": "/static/media/f5_poster_2-min.cd004a77.jpeg"
+  },
+  {
+    "revision": "0814f52be4e0808299e4c6dc4b81ab6b",
+    "url": "/static/media/fg-landing.0814f52b.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/static/media/google-play-badge.db9b21a1.png"
+  },
+  {
+    "revision": "251c7e140e31bf7c1ae93a5841a3dddb",
+    "url": "/static/media/h42 prelims result.251c7e14.jpg"
+  },
+  {
+    "revision": "69b6d40fb146ae65a6f89e7ddb187789",
+    "url": "/static/media/hacking WA.69b6d40f.png"
+  },
+  {
+    "revision": "34e22bf818798c8b7c304ef78699c996",
+    "url": "/static/media/ptb prelims result.34e22bf8.jpg"
+  },
+  {
+    "revision": "bfa8cfa9c8d8588839b9120e7eb30676",
+    "url": "/static/media/sch-min.bfa8cfa9.jpg"
+  },
+  {
+    "revision": "b31bf6cad1ea0bec43d02ad68c73c97c",
+    "url": "/static/media/sch3-min.b31bf6ca.jpg"
+  },
+  {
+    "revision": "523fd02ffac4ca3e4772db1969d222b0",
+    "url": "/static/media/sch4-min.523fd02f.jpg"
+  },
+  {
+    "revision": "8fffaa9b7b57a86a7329210a2c917980",
+    "url": "/static/media/srijan_logo_black.8fffaa9b.png"
+  },
+  {
+    "revision": "edbc62e50ce4f2f0c2a8ddc9ef844a1a",
+    "url": "/static/media/srijan_logo_white.edbc62e5.png"
+  },
+  {
+    "revision": "d1d8eae1857beb2378f881822a7e70f2",
+    "url": "/static/media/srijan_tshirt_mockup.d1d8eae1.png"
+  },
+  {
+    "revision": "b6b6626e993af46990f4ba2b9b403830",
+    "url": "/static/media/tinder-R-red-RGB.b6b6626e.png"
+  },
+  {
+    "revision": "3ca0ff72768bc46f770b1bb220cda66f",
+    "url": "/static/media/vani.3ca0ff72.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/srijan22/precache-manifest.2f877d6d781097c9f18d7bac8d25c3e3.js"
+  "/precache-manifest.a8d9ba31935f3c80bca09c5fbe392880.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/srijan22/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
